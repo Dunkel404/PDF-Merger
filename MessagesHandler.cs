c3637bd757fafb1ff9455d7be46da5ceb093668a
dunkel_404 <matheusemartins694@proton.me>
2025-02-05 15:24:54 -0300

    First commit

---
+++ MessagesHandler.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFMerger;
+public enum InputType
+{
+    StringInput,
+    SwitchInput
+}
+public enum OutputType
+{
+    Success,
+    Warning,
+    Error,
+    Information,
+}
+internal class Display
+{
+    public InputType InputType { get; set; }
+    public OutputType OutputType { get; set; }
+
+    private static void SuccessMessage (string message)
+    {
+        DisplayColoredMessage (message, ConsoleColor.Green);
+    }
+    private static void ErrorMessage (string message)
+    {
+        DisplayColoredMessage (message, ConsoleColor.Red);
+    }
+    private static void WarningMessage (string message)
+    {
+        DisplayColoredMessage (message, ConsoleColor.Yellow);
+    }
+    private static void InfoMessage (string message)
+    {
+        Console.WriteLine (message);
+    }
+
+    public static void OutputMessage (string message, OutputType outputType = OutputType.Information)
+    {
+        switch (outputType)
+        {
+            case OutputType.Warning:
+                WarningMessage (message);
+                break;
+            case OutputType.Error:
+                ErrorMessage (message);
+                break;
+            case OutputType.Information:
+                InfoMessage (message);
+                break;
+            case OutputType.Success:
+                SuccessMessage (message);
+                break;
+        }
+    }
+
+    private static bool SwitchInput (string message, bool defaultChoice)
+    {
+        string defaultLetter = defaultChoice ? "Y" : "N";
+        Console.Write (message + $"[Y / N] (Default: {defaultLetter}): ");
+        switch (Console.ReadLine ().ToLower ())
+        {
+            case "n" or "no":
+                return false;
+            case "y" or "yes":
+                return true;
+            default:
+                return defaultChoice ? true : false;
+        }
+    }
+    private static string StringInput (string message)
+    {
+        Console.Write (message);
+        return Console.ReadLine ();
+    }
+
+    /// <summary>
+    /// This method use Switch(bool) Input.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="defaultChoice"></param>
+    /// <example>You like apples? </example>
+    public static bool InputMessage (string message, bool defaultChoice)
+    {
+        return SwitchInput (message, defaultChoice);
+    }
+    /// <summary>
+    /// This method use String Input.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="defaultChoice"></param>
+    /// <example>File Input:...</example>
+    public static string InputMessage (string message)
+    {
+        return StringInput (message);
+    }
+    private static void DisplayColoredMessage (string message, ConsoleColor consoleColor)
+    {
+        ChangeForeGroundColor (consoleColor);
+        Console.WriteLine (message);
+        ChangeForeGroundColor (ConsoleColor.White);
+    }
+
+    private static void ChangeForeGroundColor (ConsoleColor consoleColor)
+    {
+        Console.ForegroundColor = consoleColor;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using PDFManager;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Fonts;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Quality;
+using PdfSharp.Snippets.Font;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace PDFMerger
+{
+    class Program
+    {
+        Display Display = new ();
+        public static void Main (string [] args)
+        {
+            if (Capabilities.Build.IsCoreBuild)
+            {
+                GlobalFontSettings.FontResolver = new FailsafeFontResolver ();
+            }
+            StartMergeProcess ();
+        }
+
+        public static void StartMergeProcess ()
+        {
+            List<PdfDocument> fileList = new List<PdfDocument> ();
+
+            Display.OutputMessage ("Accepted document extensions: .pdf .png .jpg .bmp", OutputType.Warning);
+
+            GetDocuments (fileList);
+
+            MergeDocuments (fileList, out PdfDocument outputDocument);
+
+            bool isRelativeToFirstDocument = Display.InputMessage ("Save in the same path as the first document?", true);
+            if (isRelativeToFirstDocument)
+            {
+                string path = fileList [0].IsImported ?
+                    new FileInfo (fileList [0].FullPath).Directory.FullName :
+                    new FileInfo (fileList [0].Info.Comment).Directory.FullName;
+
+                Display.OutputMessage ("path: " + path, OutputType.Success);
+                SaveOutput (outputDocument, true, path);
+            } else
+                SaveOutput (outputDocument, false);
+        }
+        public static void GetDocuments (List<PdfDocument> fileList)
+        {
+            bool inputingFiles = true;
+            while (inputingFiles)
+            {
+                string newDocumentPath = @"" + Display.InputMessage ("Input file path: ");
+                newDocumentPath = newDocumentPath.Replace ('\\', '/').Trim ('"');
+
+                try
+                {
+
+                    if (string.IsNullOrEmpty (newDocumentPath))
+                    {
+                        inputingFiles = false;
+                    } else if (File.Exists (newDocumentPath))
+                    {
+                        PdfDocument newDocument;
+                        switch (Path.GetExtension (newDocumentPath))
+                        {
+                            case ".png" or ".bmp" or ".jpg" or ".jpeg":
+                                Image pathImage = Image.FromFile (newDocumentPath);
+                                XImage newImage;
+                                using (MemoryStream ms = new ())
+                                {
+                                    pathImage.Save (ms, ImageFormat.Png);
+                                    byte [] imageBytes = ms.ToArray ();
+                                    newImage = XImage.FromStream (ms);
+                                }
+                                newDocument = new PdfDocument (new FileInfo (newDocumentPath).Directory.FullName + "/new_fileWithImage.pdf");
+                                PdfPage page1 = newDocument.AddPage ();
+
+                                page1.Width = XUnitPt.FromPoint (newImage.PointWidth);
+                                page1.Height = XUnit.FromPoint (newImage.PointHeight);
+                                XGraphics gfx = XGraphics.FromPdfPage (page1);
+                                newDocument.Info.Comment = new FileInfo (newDocumentPath).Directory.FullName + "/new_fileWithImage.pdf";
+                                gfx.DrawImage (newImage, 0, 0, newImage.PixelWidth, newImage.PixelHeight);
+                                fileList.Add (newDocument);
+                                newDocument.Close ();
+                                Display.OutputMessage (new FileInfo (newDocumentPath).Name + " - Successfully added to merge list!", OutputType.Success);
+                                break;
+                            case ".pdf":
+                                newDocument = PdfReader.Open (newDocumentPath, PdfDocumentOpenMode.Import);
+                                fileList.Add (newDocument);
+                                Display.OutputMessage (new FileInfo (newDocumentPath).Name + " - Successfully added to merge list!", OutputType.Success);
+                                break;
+                            default:
+                                Display.OutputMessage (("Warning: File extension not supported! - " + Path.GetExtension (newDocumentPath)), OutputType.Warning);
+                                break;
+                        }
+
+                    } else { throw new FileNotFoundException ($"File {newDocumentPath}, not found "); }
+                } catch (Exception ex)
+                {
+                    Display.OutputMessage (ex.Message, OutputType.Error);
+                }
+            }
+        }
+        public static void MergeDocuments (List<PdfDocument> fileList, out PdfDocument outputDocument)
+        {
+            outputDocument = new PdfDocument ();
+            for (int fileIdx = 0 ;fileIdx < fileList.Count ;fileIdx++)
+            {
+                PdfDocument specificFile = fileList [fileIdx];
+                bool isImported = specificFile.IsImported;
+                if (isImported)
+                {
+                    for (int pageIdx = 0 ;pageIdx < specificFile.PageCount ;pageIdx++)
+                    {
+
+                        outputDocument.AddPage (specificFile.Pages [pageIdx]);
+                    }
+                } else
+                {
+                    outputDocument.AddPage (PdfReader.Open (specificFile.Info.Comment, PdfDocumentOpenMode.Import).Pages [0]);
+                }
+            }
+
+            Display.OutputMessage ($"Successfully merged {fileList.Count} documents!", OutputType.Success);
+        }
+        public static void SaveOutput (PdfDocument documentOutput, bool relativeTo, string outputFilePath = "")
+        {
+            string outputFileName = @"" + Display.InputMessage ("output file name: ");
+            outputFileName = string.IsNullOrEmpty (outputFileName) ? "New_File" + DateTime.Now.ToString ("dd_MM_yy ss-mm-HH") : outputFileName;
+            if (!relativeTo)
+            {
+                outputFilePath = Display.InputMessage ("output file path: ");
+            }
+
+            string savePath = outputFilePath + $"/{outputFileName}.pdf";
+            Display.OutputMessage ("Success! File saved at: " + savePath, OutputType.Success);
+            documentOutput.Save (savePath);
+            documentOutput.Close ();
+            if (Display.InputMessage ("Open the file's folder?", false))
+            {
+                Process.Start ("explorer.exe", outputFilePath);
+            }
+            if (Display.InputMessage ("Restart process? ", true))
+            {
+                StartProcess ();
+            } else 
+                Environment.Exit (0);
+        }
+    }
+}
